04fe8b52ab15bab91570bd53abca69488310587e
Ryan Le <91763551+ryanvle@users.noreply.github.com>
2022-10-31 17:47:23 -0700

    Add files via upload

---
+++ src/PageFooter.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function PageFooter() {
+  return (
+      <footer className="footer">
+          <p>Author: co-authored by: Jackie Chen, Jonathan Kim, Ryan Le, Matthew Bacarro</p>
+          <p><a href="Support@HuskyHelp.com">Support@HuskyHelp.com</a></p>
+      <footer>&copy; Copyright 2022 HTML.am</footer>
+      </footer>
+  );
+}
